66bcd43357caf4f3f401eeb95a7e74141adcb778
MOHAMMED SHOEBUDDIN HABEEB <f2014895@hyderabad.bits-pilani.ac.in>
2019-11-06 16:07:37 -0500

    Add files via upload

---
+++ HW1_P2.py.py
@@ -0,0 +1,8 @@
+New_str = input("Please input a string: ")
+print(New_str)
+L = len(New_str)
+print("Number of words :",L)
+if (L%2==0):
+	print("It is an even number")
+else:
+	print("It is an odd number")
